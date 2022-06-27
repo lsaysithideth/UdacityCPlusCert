6af1887a46bcf45a90e6ebad1399d1bdaa68ebb3
Linda Saysithideth <59379166+lsaysithideth@users.noreply.github.com>
2022-06-26 20:20:02 -0400

    Add files via upload

---
+++ vectorBoard.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <vector>
+using std::cout;
+using std::vector;
+
+int main(){
+    std::vector<std::vector<int>> board {{0, 1, 0, 0, 0, 0},
+{0, 1, 0, 0, 0, 0},
+{0, 1, 0, 0, 0, 0},
+{0, 1, 0, 0, 0, 0},
+{0, 0, 0, 0, 1, 0}};
+cout << "Finished\n";
+}
